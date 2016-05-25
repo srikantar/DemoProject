058b33e2df0430bf25f55bb8a74746c8c1026f05
Joe Olson <jolson7168@nododos.com>
2016-05-25 09:13:49 -0500

    updated

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# MoreySimulator
+# Morey Simulator
+
+This purpose of this project is to simulate a [Morey Connect MC-3 device](http://www.moreycorp.com/expertise/technologies/wireless-devices/morey-connect/).
+
+## Background
+
+The [Morey Connect MC-3 device](http://www.moreycorp.com/expertise/technologies/wireless-devices/morey-connect/) is an embedded hardware device that connects to a vehicle's OBD2 port. When the vehicle's ignition is in the 'run' position, the MC-3 will receive power, and will be collecting ODB2 message traffic (J1939, J1708, VPW & PWM, KW2000, ISO 9141). The MC-3 also contains a cellular transciever, and can report this message traffic via UDP to a listening server.
+
+In order to simulate this (initially), an actual MC-3 device is used to transmit real data from a real vehicle to a real listener. The real listener is modified to dump the byte stream sent by the MC-3 to a file on disk. This file can then be used to "replay" the real data back to a listener via a Python app at a later date.
+
+As the project gets more sophisticated, it will move away from the "record-and-playback" model to a better "generate data on the fly" model.
+
+## Installation
+
+The project consists of:
+    - a Python playback application: ./src/simulateMorey.py
+    - sample replay data: ./data/trip1.txt
+    - a configuration file: ./config/morey.conf_template
+
+Rename morey.conf_template to morey.conf, and fill in the values for the listener. You can execute via ./scripts/simulateMorey.sh
+
+## Operation
+
+All project logs will be stored in the ./logs directory.
+
+When the task starts, it will begin parsing the replay data file. It will simulate the MC-3 by sending the byte streams represented in the file to the listener in real time, following the real time offsets of the timestamps in the file. e.g.:
+
+```
+# Mon May 23 01:53:31 2016 - ignition on
+15020001007d006600a1000021d18b220000000[...]900000000106c000000003027ffff3026ffffa0380000
+
+# Mon May 23 01:54:41 2016 - in motion
+15020001005f006700574262fb2007101501d1a[...]e000003bf502dff502eff301a0446301b041f401c0051
+```
+The second byte stream will be transmitted one minute and 10 seconds after the first. 
+
+The original timestamps embedded in the byte stream (see the MC-3 documentation) will be overriden with the current time. This will make the trip appear to be happening in real time.
+
+## Problems
+
+Only the first timestamp in a multi-message stream is getting replaced with the current timestamp.
+
--- src/simulateMorey.py
+++ src/simulateMorey.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
-from __future__ import print_function      # get latest print functionality
 import logging
 import sys
 import time
+import socket
+import base64
 from ConfigParser import RawConfigParser
+from datetime import datetime
 
 cfg = RawConfigParser()
 
@@ -17,9 +19,7 @@ def currentTimeStr():
 def initLog(rightNow):
     logger = logging.getLogger(cfg.get('logging', 'logName'))
     logPath=cfg.get('logging', 'logPath')
-    logFilename=cfg.get('logging', 'logFileName')
-    print(logPath)
-    print(logFilename)    
+    logFilename=cfg.get('logging', 'logFileName')  
     hdlr = logging.FileHandler(logPath+rightNow+logFilename)
     formatter = logging.Formatter(cfg.get('logging', 'logFormat'),cfg.get('logging', 'logTimeFormat'))
     hdlr.setFormatter(formatter)
@@ -39,14 +39,71 @@ def getCmdLineParser():
 
     return parser
 
+# Convert the text time stamp to a timestamp
+def getDate(line):
+    if "-" in line:
+        return datetime.strptime(line[2:line.index("-")-1].rstrip(),'%a %b %d %I:%M:%S %Y')
+    else:
+        return datetime.strptime(line[2:].rstrip(),'%a %b %d %I:%M:%S %Y')
+
+# This replaces the original timestamp in the data with the current one.
+def useCurrentDate(line, mask):
+    start=line.index(mask)
+    current=line[start:start+8]
+    line=line.replace(current,str(hex(int(round(time.time()))))[2:])
+    return(line)
+
+# This is the logic to figure out the original timestamp of the data in the scenario file.
+def getOriginalTime(line):
+    start = 48
+    if "|" in line:
+        start = start +1
+    return(line[20+start:20+start+4])
+        
+# Iterate through the scenario file, replace the old timestamps with the current one, and transmit.
+def executeScenario(scenarioFile, sock, logger):
+    file = open(scenarioFile, 'r')
+    currentTime = None
+    lastTime = None
+    waitTime = 0
+    for line in file:
+        if line[0]=="#":
+            lastTime = currentTime
+            currentTime = getDate(line)
+            if lastTime == None:
+                waitTime = 0
+            else:
+                waitTime = (currentTime - lastTime).total_seconds()
+        elif len(line)>100:
+            if waitTime == 0:
+                replaceDate = getOriginalTime(line)
+            time.sleep(waitTime)
+            textPackage = useCurrentDate(line.replace("|",""),replaceDate).rstrip()
+            logger.info("Xmit: "+textPackage) 
+            package = textPackage.decode("hex")
+            sock.sendto(package, (cfg.get('listener', 'UDP_IP'), cfg.getint('listener', 'UDP_PORT')))
+    # Fix this. Not critical.
+    file.close()     
 
 def main(argv):
+
+    # Overhead to manage command line opts and config file
     p = getCmdLineParser()
     args = p.parse_args()
     cfg.read(args.config_file)
+
+    # Get the logger going
     rightNow = time.strftime("%Y%m%d%H%M%S")
     logger = initLog(rightNow)
-    logger.info('Starting Run: '+currentDayStr()+'  ==============================')
+    logger.info('Starting Run: '+time.strftime("%Y%m%d%H%M%S")+'  ==============================')
+   
+    # Set up the sockets
+    sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+    # Execute the scenario file
+    executeScenario(args.scenario_file, sock, logger)
+
+    # Clean up
     logger.info('Done! '+time.strftime("%Y%m%d%H%M%S")+'  ==============================')
 
 if __name__ == "__main__":
